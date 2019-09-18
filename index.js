7f09c8a4ee892e6f93e01070fbf934a324148ff6
Martin Cervenka <martincervenka10@gmail.com>
2019-09-18 12:10:21 +0200

    websocket

--- index.js
+++ index.js
@@ -35,18 +35,13 @@ const ws = new WebSocket('wss://boiling-sands-96880.herokuapp.com', {
 });
 
 ws.onopen = function open() {
-  console.log('connected');
-  ws.send(Date.now());
 };
 
 ws.onclose = function close() {
-  console.log('disconnected');
 };
 
 ws.onmessage = function incoming(data) {
-  console.log(`Roundtrip time: ${Date.now() - data.data} ms`);
-
-  setTimeout(function timeout() {
-    ws.send(Date.now());
-  }, 500);
+  wss.clients.forEach(function each(client) {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send(data);
 };
