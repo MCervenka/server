252927dd6938e89fbda10669e03d07f678cab672
Martin Cervenka <martincervenka10@gmail.com>
2019-09-18 08:49:58 +0200

    websocket

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 let WSServer = require('ws').Server;
 let server = require('http').createServer();
-let app = require('./httpServer');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+const bodyParser = require("body-parser");
+require("./models/User");
+require("./services/passport");
+require("./models/Comments");
+require("./models/Events");
+
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+require("./routes/commentRoutes")(app);
+require("./routes/eventRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 // Create web socket server on top of a regular http server
 let wss = new WSServer({
