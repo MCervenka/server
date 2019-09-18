7fea63ca4aaae6e1d57fa5164d29bcac9cb7c6b7
Martin Cervenka <martincervenka10@gmail.com>
2019-09-18 09:05:20 +0200

    websocket

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
 let WSServer = require('ws').Server;
 let server = require('http').createServer();
-const express = require("express");
-const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-const keys = require("./config/keys");
-const bodyParser = require("body-parser");
-require("./models/User");
-require("./services/passport");
-require("./models/Comments");
-require("./models/Events");
-
-mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
-
-const app = express();
-app.use(bodyParser.json());
-app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-require("./routes/authRoutes")(app);
-require("./routes/billingRoutes")(app);
-require("./routes/commentRoutes")(app);
-require("./routes/eventRoutes")(app);
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-
-    const path = require("path");
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
+let app = require('./httpServer');
 
 // Create web socket server on top of a regular http server
 let wss = new WSServer({
@@ -62,3 +26,27 @@ server.listen(PORT, function() {
 
   console.log(`http/ws server listening on ${process.env.PORT}`);
 });
+
+const WebSocket = require('isomorphic-ws')
+
+
+const ws = new WebSocket('wss://boiling-sands-96880.herokuapp.com', {
+  origin: 'https://boiling-sands-96880.herokuapp.com'
+});
+
+ws.onopen = function open() {
+  console.log('connected');
+  ws.send(Date.now());
+};
+
+ws.onclose = function close() {
+  console.log('disconnected');
+};
+
+ws.onmessage = function incoming(data) {
+  console.log(`Roundtrip time: ${Date.now() - data.data} ms`);
+
+  setTimeout(function timeout() {
+    ws.send(Date.now());
+  }, 500);
+};
