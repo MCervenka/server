047690c492c981016c62e564e284c6fc39fd29a0
Martin Cervenka <martincervenka10@gmail.com>
2019-09-27 10:00:45 +0200

    Landing text

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,14 +1,18 @@
 import React from "react";
-
+const textUvod = "Jmenuji se Katka Červenková a budu pečovat o Vaše přírodní nechty dle Vašich potřeb a přání";
+const textOdsek1 = "Péče o ruce a nehty mě fascinovala už jako malou holku. Více jsem se začala věnovat tomuto oboru přibližně před 12 lety, kdy jsem zkoušela různé materiály a zdobení.";
+const textOdsek2 = "Už tehdy jsem pracovala s technikou gelových nehtů a v roku 2014 jsem absolvovala kurz akrylovej modeláže. Dnes jsou to už 3 roky, co se specializuji na léčbu přírodních nehtů, zničených po nehtové modeláži nebo neodborně provedenou manikúrou. Mým cílem je řešit tyto problémy komplexně, přes vitamíny, stravu a následnou domácí péči o nehty.";
 
 const Landing = () => {
     return (
-    <div style={{textAlign: "center"}}>
-        <h1>
-            Salon
+        <div style={{ textAlign: "center" }}>
+            <h1>
+                Salon
         </h1>
-        Vitajte v Katkinom salone.
-    </div>  
+            <p> {textUvod} </p>
+            <p> {textOdsek1} </p>
+            <p> {textOdsek2} </p>
+        </div>
     );
 };
 
